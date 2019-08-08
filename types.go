3d972a39b928c61dabef2260e2ee86eb9f3b4a87
shootacean <shootacean@gmail.com>
2019-08-08 22:53:20 +0900

    選択した要素が依存している要素群を表示できるようにする

--- types.go
+++ types.go
@@ -27,7 +27,7 @@ func (types *ItemTypes) Fetch() error {
 		return errQuery
 	}
 	defer rows.Close()
-
+	
 	for rows.Next() {
 		i := ItemType{}
 		if err := rows.Scan(&i.Id, &i.Name); err != nil {
@@ -39,11 +39,9 @@ func (types *ItemTypes) Fetch() error {
 }
 
 type Item struct {
-	Id          sql.NullInt64  `json:"id"`
-	Name        sql.NullString `json:"name"`
-	Description sql.NullString `json:"description"`
-	TypeId      sql.NullInt64  `json:"typeId"`
-	TypeName    sql.NullString `json:"typeName"`
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
 
 func (item *Item) Fetch(id int) error {
@@ -52,15 +50,15 @@ func (item *Item) Fetch(id int) error {
 		fmt.Printf("%v", err)
 		return err
 	}
-	rows, errQuery := db.Query("SELECT id, name, type_id FROM items WHERE id = $1", id)
+	rows, errQuery := db.Query("SELECT id, name, description FROM items WHERE id = $1", id)
 	if errQuery != nil {
 		fmt.Printf("%v", errQuery)
 		return errQuery
 	}
 	defer rows.Close()
-
+	
 	for rows.Next() {
-		if err := rows.Scan(&item.Id, &item.Name, &item.TypeId); err != nil {
+		if err := rows.Scan(&item.Id, &item.Name, &item.Description); err != nil {
 			fmt.Printf("%v", err)
 			return err
 		}
@@ -77,15 +75,15 @@ func (items *Items) Fetch() error {
 	if err != nil {
 		return err
 	}
-	rows, errQuery := db.Query("select id, name, type_id from items")
+	rows, errQuery := db.Query("select id, name, description from items")
 	if errQuery != nil {
 		return errQuery
 	}
 	defer rows.Close()
-
+	
 	for rows.Next() {
 		i := Item{}
-		if err := rows.Scan(&i.Id, &i.Name, &i.TypeId); err != nil {
+		if err := rows.Scan(&i.Id, &i.Name, &i.Description); err != nil {
 			return err
 		}
 		items.Data = append(items.Data, i)
@@ -95,7 +93,7 @@ func (items *Items) Fetch() error {
 
 type ItemDependency struct {
 	Item
-	DepReason sql.NullString `json:"depReason"`
+	Reason string `json:"reason"`
 }
 
 type ItemDependencies struct {
@@ -111,16 +109,13 @@ func (itemDeps *ItemDependencies) Fetch(itemId int) error {
 	}
 	rows, errQuery := db.Query(`
 		SELECT
-			-- items.id,
-       		-- item_types.name as type_name,
-       		-- items.name,
        		item_dependencies.item_dest_id AS id,
        		dependency_items.name AS name,
        		dependency_items.description AS description,
-       		item_dependencies.reason AS dep_reason
+       		item_dependencies.reason AS reason
 		FROM items
        		LEFT JOIN item_types ON item_types.id = items.type_id
-       		LEFT JOIN item_dependencies ON item_dependencies.item_id = items.id
+       		INNER JOIN item_dependencies ON item_dependencies.item_id = items.id
        		LEFT JOIN items dependency_items ON dependency_items.id = item_dependencies.item_dest_id
        		LEFT JOIN item_types dependency_item_types ON dependency_item_types.id = dependency_items.type_id
 		WHERE items.id = $1
@@ -130,10 +125,10 @@ func (itemDeps *ItemDependencies) Fetch(itemId int) error {
 		return errQuery
 	}
 	defer rows.Close()
-
+	
 	for rows.Next() {
 		i := ItemDependency{}
-		if err := rows.Scan(&i.Id, &i.Name, &i.Description, &i.DepReason); err != nil {
+		if err := rows.Scan(&i.Id, &i.Name, &i.Description, &i.Reason); err != nil {
 			fmt.Printf("%v", err)
 			return err
 		}
