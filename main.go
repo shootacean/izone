b89aa5821c037bc6f2fcdf7e548251c638da2fec
Shota Tsukamoto <s-tsukamoto@tsgroup.jp>
2019-08-08 19:49:04 +0900

    作業途中

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	http.HandleFunc("/item_types", itemTypesHandler)
 	http.HandleFunc("/items", itemsHandler)
-	http.HandleFunc("/dependency_items", dependencyItemsHandler)
+	http.HandleFunc("/item_dependencies", itemDependenciesHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
@@ -54,20 +56,42 @@ func itemsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func dependencyItemsHandler(w http.ResponseWriter, r *http.Request) {
-	types := DependencyItems{}
-	if err := types.Fetch(); err != nil {
-		http.Error(w, "Failed fetch", http.StatusInternalServerError)
+func itemDependenciesHandler(w http.ResponseWriter, r *http.Request) {
+	queries := r.URL.Query()
+	itemId := queries.Get("itemId")
+	if itemId == "" {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
-	j, errJson := json.Marshal(types.Data)
+	id, _ := strconv.Atoi(itemId)
+
+	itemDeps := ItemDependencies{}
+	if err := itemDeps.Fetch(id); err != nil {
+		fmt.Printf("%v", err)
+		http.Error(w, "Failed fetch dependencies", http.StatusInternalServerError)
+		return
+	}
+
+	item := Item{}
+	if err := item.Fetch(id); err != nil {
+		fmt.Printf("%v", err)
+		http.Error(w, "Failed fetch items", http.StatusInternalServerError)
+		return
+	}
+	itemDeps.Id = item.Id
+	itemDeps.Name = item.Name
+	itemDeps.Description = item.Description
+
+	j, errJson := json.Marshal(itemDeps)
 	if errJson != nil {
+		fmt.Printf("%v", errJson)
 		http.Error(w, "Failed marshall json", http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	if _, err := w.Write(j); err != nil {
+		fmt.Printf("%v", err)
 		http.Error(w, "Failed response json", http.StatusInternalServerError)
 		return
 	}
--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,11 @@
 import Browser
 import Html exposing (..)
+import Html.Events exposing (onClick)
 import Http
-import Json.Decode exposing (Decoder, field, string)
+import Json.Decode exposing (Decoder)
 import Debug
+
+
 
 main =
     Browser.element
@@ -12,51 +15,129 @@ main =
       , view = view
       }
 
+
+
 type Model
   = Failure
   | Loading
-  | Success (List ItemType)
+  | SuccessItemTypes (List ItemType)
+  | SuccessItems (List Item)
+  | SuccessDependencyItems (List DependencyItem)
+
+
 
 type alias ItemType =
     { id : Int, name : String }
 
+type alias Item =
+    { id : Int, name : String, description : String, typeId : Int, typeName : String }
+
+--type alias DependencyItem =
+--    { id : Int, name : String }
+
+
+
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Loading, getItemTypes )
+  ( Loading, getItems )
+
 
 
 type Msg
-  = GotItemTypes (Result Http.Error (List ItemType))
+  = GetItemTypes
+  | GotItemTypes (Result Http.Error (List ItemType))
+  | GetItems
+  | GotItems (Result Http.Error (List Item))
+  | GetDependencyItems
+  | GotDependencyItems (Result Http.Error (List DependencyItem))
+  | GetItemDependencies Int
+
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
+    GetItemTypes ->
+        ( model, getItemTypes )
+
+    GetItems ->`
+        ( model, getItems )
+
+    GetDependencyItems ->
+        ( model, getDependencyItems )
+
+    GetItemDependencies itemId ->
+        ( model, getItemDependencies itemId )
+
     GotItemTypes result ->
       case result of
         Ok fullText ->
-          (Success fullText, Cmd.none)
-
+          (SuccessItemTypes fullText, Cmd.none)
         Err err ->
           Debug.log ( Debug.toString err )
           (Failure, Cmd.none)
+
+    GotItems result ->
+      case result of
+        Ok fullText ->
+          (SuccessItems fullText, Cmd.none)
+        Err err ->
+          Debug.log ( Debug.toString err )
+          (Failure, Cmd.none)
+
+    GotDependencyItems result ->
+          case result of
+            Ok fullText ->
+              (SuccessDependencyItems fullText, Cmd.none)
+            Err err ->
+              Debug.log ( Debug.toString err )
+              (Failure, Cmd.none)
+
 
 
 view : Model -> Html Msg
 view model =
     case model of
         Loading ->
-          text "Loading..."
+          div [] [ text "Loading..." ]
 
         Failure ->
-          text "I was unable to load your resource."
+          div [] [ text "I was unable to load your resource." ]
 
-        Success itemTypes ->
+        SuccessItemTypes itemTypes ->
           div []
-              [ table []
-                      [ tbody [] ( viewItemTypes itemTypes ) ]
+              [ viewMenu
+              , table []
+                      [ tbody []
+                              ( viewItemTypes itemTypes )
+                      ]
               ]
 
-viewItemTypes : (List ItemType) -> List (Html msg)
+        SuccessItems items ->
+          div []
+              [ viewMenu
+              , table []
+                      [ tbody []
+                              ( viewItems items )
+                      ]
+              ]
+
+        SuccessDependencyItems dependencyItems ->
+          div []
+              [ viewMenu
+              , table []
+                      [ tbody []
+                              ( viewDependencyItems dependencyItems )
+                      ]
+              ]
+
+viewMenu : Html Msg
+viewMenu =
+  div [] [ button [ onClick GetItems ] [ text "Items" ]
+         , button [ onClick GetItemTypes ] [ text "ItemTypes" ]
+         , button [ onClick GetDependencyItems ] [ text "DependencyItems" ]
+         ]
+
+viewItemTypes : (List ItemType) -> List (Html Msg)
 viewItemTypes itemTypes =
   List.map
     (\itemType ->
@@ -66,6 +147,33 @@ viewItemTypes itemTypes =
          ]
     )
     itemTypes
+
+viewItems : (List Item) -> List (Html Msg)
+viewItems items =
+  List.map
+    (\item ->
+      tr []
+         [ td [] [ text ( String.fromInt item.id ) ]
+         , td [] [ text item.name ]
+         , td [] [ text ( String.fromInt item.typeId ) ]
+         , td [] [ button [ onClick ( GetItemDependencies item.id )]
+                          [ text "dependencies" ]
+                 ]
+         ]
+    )
+    items
+
+viewDependencyItems : (List DependencyItem) -> List (Html Msg)
+viewDependencyItems dependencyItems =
+  List.map
+    (\dependencyItem ->
+      tr []
+         [ td [] [ text ( String.fromInt dependencyItem.id ) ]
+         , td [] [ text dependencyItem.name ]
+         ]
+    )
+    dependencyItems
+
 
 
 getItemTypes : Cmd Msg
@@ -84,6 +192,54 @@ itemTypeDecoder =
 itemTypesDecoder : Decoder (List ItemType)
 itemTypesDecoder =
     Json.Decode.list itemTypeDecoder
+
+
+getItems : Cmd Msg
+getItems =
+    Http.get
+      { url = "http://localhost:8080/items"
+      , expect = Http.expectJson GotItems itemsDecoder
+      }
+
+itemDecoder : Decoder Item
+itemDecoder =
+  Json.Decode.map5 Item
+    (Json.Decode.field "id" Json.Decode.int)
+    (Json.Decode.field "name" Json.Decode.string)
+    (Json.Decode.field "description" Json.Decode.string)
+    (Json.Decode.field "typeId" Json.Decode.int)
+    (Json.Decode.field "typeName" Json.Decode.string)
+
+itemsDecoder : Decoder (List Item)
+itemsDecoder =
+    Json.Decode.list itemDecoder
+
+
+getDependencyItems : Cmd Msg
+getDependencyItems =
+    Http.get
+      { url = "http://localhost:8080/item_dependencies"
+      , expect = Http.expectJson GotDependencyItems dependencyItemsDecoder
+      }
+
+dependencyItemDecoder : Decoder DependencyItem
+dependencyItemDecoder =
+  Json.Decode.map2 DependencyItem
+    (Json.Decode.field "id" Json.Decode.int)
+    (Json.Decode.field "name" Json.Decode.string)
+
+dependencyItemsDecoder : Decoder (List DependencyItem)
+dependencyItemsDecoder =
+    Json.Decode.list dependencyItemDecoder
+
+
+getItemDependencies : Int -> Cmd Msg
+getItemDependencies itemId =
+    Http.get
+      { url = String.concat ["http://localhost:8080/item_dependencies?itemId=", String.fromInt(itemId) ]
+      , expect = Http.expectJson GotDependencyItems dependencyItemsDecoder
+      }
+
 
 
 subscriptions : Model -> Sub Msg
